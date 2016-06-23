ec9a9835cee067e9d5568aa6a9326254a26ced99
fidelscodes <fidelscodes@gmail.com>
2016-06-23 12:58:54 -0400

    Update with solution

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+require "pry"
+class Dog
+  # setter method
+  def name=(name)
+    @name = name
+  end
+
+  #getter method
+  def name
+    @name
+  end
+
+  def bark
+    puts "woof!"
+  end
+end
+# binding.pry
