96e2b195daa6050e5b1263ba855cbbdd307e89e8
parm530 <parmsahadeo530@gmail.com>
2016-07-10 21:22:22 -0400

    Created Dog class with instance methods: .name, .name= and .bark

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Dog
+
+  def name= (name)  #setter method
+    @name = name
+  end
+
+  def name          #getter method
+    @name
+  end
+
+  def bark          #bark method
+    puts "woof!"
+  end
+
+end
+
+
